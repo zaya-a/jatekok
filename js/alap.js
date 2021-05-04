26beff88a2c7eb92ef49fcaabe9ca23ff530f003
DrTlKl <cs.zsombi03@gmail.com>
2021-05-04 13:27:50 +0200

    alap weblapszerkezet minta alapján

--- js/alap.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load",myInit,false);
-
-function $(id){
-    document.getElementById(id);
-}
---
+++ js/code.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", init, false);
+
+function $(id){
+    return document.getElementById(id);
+}
+
+function init(){
+
+}
