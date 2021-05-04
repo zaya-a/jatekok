8d9667d07a3f678e74267350f2dd7c72a4d824d1
DrTlKl <cs.zsombi03@gmail.com>
2021-05-04 16:45:00 +0200

    Kész a játék
    alapvetően kész a játékom már csak apróságokat kell hozzáadnom. csiszolni kell rajta

--- js/zscode.js
+++ js/zscode.js
@@ -14,23 +14,71 @@ function init(){
     startgomb.addEventListener("click", mainGame, false);
 }
 
-let selected = 3;
+/*
+TODO
+scoreboard
+difficulty
+hud
+keyboard control
+*/
+
+let player = 3;
+let delay=1500;
 
 function leftmove(){
-    if(selected > 1){
-        $("p"+(selected)).style.backgroundColor="black";
-        $("p"+(--selected)).style.backgroundColor="white";
+    if(player > 1){
+        $("p"+(player)).style.backgroundColor="black";
+        $("p"+(--player)).style.backgroundColor="white";
     }
 }
 
 function rightmove(){
-    if(selected < 5){
-        $("p"+(selected)).style.backgroundColor="black";
-        $("p"+(++selected)).style.backgroundColor="white";
+    if(player < 5){
+        $("p"+(player)).style.backgroundColor="black";
+        $("p"+(++player)).style.backgroundColor="white";
     }
 }
 
 function mainGame(){
     console.log("komo is sus, m8 be impasta");
+    delay=1500;
+    redbox();
 }
 
+function redbox(){
+    for(let tempx = 1; tempx < 6; tempx++){
+        for(let tempy = 1; tempy < 6; tempy++){
+            $("x"+tempx+"y"+tempy).style.backgroundColor="black";
+        }
+    }
+    let col = Math.floor(Math.random()*5+1);
+    let row = 1;
+
+    $("x"+row+"y"+col).style.backgroundColor="red";
+
+   nextredbox();
+
+   setTimeout(function(){
+       if(player==col){
+            delay=delay*0.85;
+            redbox();
+       }else{
+           window.alert("Vége");
+       }
+   }, delay*5);
+
+   function nextredbox(){
+       if(row < 4){
+            setTimeout(function(){
+                $("x"+row+"y"+col).style.backgroundColor="black";
+                $("x"+ (++row) + "y" + col).style.backgroundColor="red";
+                nextredbox();
+            },delay);
+        }else{
+            setTimeout(function(){
+                $("x"+row+"y"+col).style.backgroundColor="black";
+                $("x"+ (++row) + "y" + col).style.backgroundColor="red";
+            },delay);
+        }
+   }
+}
