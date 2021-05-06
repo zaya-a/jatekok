e3c06c8c4d60cc3ce8aeed04fb2c25a330f65adf
Tibor Aux <tiboraux@gmail.com>
2021-05-06 22:10:41 +0200

    Szépítgetés.

--- js/szinvalaszto.js
+++ js/szinvalaszto.js
@@ -3,23 +3,33 @@ function $(id){
     return document.getElementById(id);
 }
 function init(){
- $("startGomb").addEventListener("click",fugveny,false);
+    
+ $("startGomb").addEventListener("click",start,false);
  $("szin1").addEventListener("click",szin1,false);
  $("szin2").addEventListener("click",szin2,false);
  $("szin3").addEventListener("click",szin3,false);
  $("szin4").addEventListener("click",szin4,false);
  $("szin5").addEventListener("click",szin5,false);
  $("szin6").addEventListener("click",szin6,false);
+ $("newGame").addEventListener("click",newGame,false);
 }
 var rgbRandom= "";
-let fugveny = function(){
+let start = function(){
     //randomrgb
+    $("szinek").style.visibility = "visible";   
     let red = Math.floor(Math.random() * (255 - 0) + 0);
     let green = Math.floor(Math.random() * (255 - 0) + 0);
     let blue = Math.floor(Math.random() * (255 - 0) + 0);
 
     console.log("Az rgb kód:" + red + green + blue);
-    $("rgbMutato").innerHTML = "Az rgb kód: " + red+" "+ green+" "+ blue; 
+    $("rgbMutato").innerHTML = "Az rgb kód: " + '<span id="piros">'+red+'</span>'+" "+ '<span id="zöld">'+green+'</span>'+" "+ '<span id="kek">'+blue+'</span>'; 
+    $("piros").style.color="red";
+    $("piros").style.fontWeight=500;
+    $("zöld").style.color="green";
+    $("zöld").style.fontWeight=500;
+    $("kek").style.color="blue";
+    $("kek").style.fontWeight=500;
+
     //rgb a köröknek
     for( let i = 1; i<=6; i++ ){
         let r = Math.floor(Math.random() * (255 - 0) + 0);
@@ -34,36 +44,50 @@ let fugveny = function(){
     $("szin"+kivétel).style.backgroundColor = rgbRandom;
     console.log($("szin1").style.backgroundColor);
     console.log(rgbRandom);
+    $("startGomb").style.visibility = "hidden";
+    $("startGomb").style.transition = "0s";
+    $("newGame").style.transition = "0s";
     
 }
 
 function szin1(){
     if($("szin1").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
 }
 function szin2(){
     if($("szin2").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
 }
 function szin3(){
     if($("szin3").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
 }
 function szin4(){
     if($("szin4").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
 }
 function szin5(){
     if($("szin5").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
 }
 function szin6(){
     if($("szin6").style.backgroundColor == rgbRandom){
         $("result").innerHTML = "Gratulálok, megtaláltad a színt!";
+        $("newGame").style.visibility = "visible";
     }
+}
+function newGame(){
+    start();
+    $("result").innerHTML = "";
+    $("newGame").style.visibility = "hidden";
 }
